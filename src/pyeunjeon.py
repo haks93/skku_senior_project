ef6f719f3a8e3b5dfbcd286a46985ed32034cda5
yusinshin <yusinshin@gmail.com>
2019-02-13 22:25:00 +0900

    2018/korean 자료 수집, txt변환 끝.
    3.nouns/korean.txt 에 빈도수 내림차순으로 새로 저장.

--- src/pyeunjeon.py
+++ src/pyeunjeon.py
@@ -1,5 +1,6 @@
 from eunjeon import Mecab
 import os
+import operator
 
 def pyeunjeon(subjectIndex):
     # 0: kor, 1: eng,  2: math, 3: physics, 4:chemical, 5: bio, 6: earth
@@ -11,29 +12,40 @@ def pyeunjeon(subjectIndex):
     tagger = Mecab()
     hash = {}
 
-    for filename in os.listdir(os.getcwd()):
-        if filename.endswith(".txt"):
-            f = open(filename, "r", encoding='utf-8')
-            data = f.read()
-            words_pos = tagger.pos(data)
+    for filename2 in  os.listdir(os.getcwd()):
+        os.chdir("./" + filename2)
 
-            for word in words_pos:
-                noun = word[0]
-                category = word[1]
-                if (category == 'NNG'):
-                    if noun in hash:
-                        hash[noun] = hash[noun] + 1
-                    else:
-                        hash[noun] = 1
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith(".txt"):
+                print(os.getcwd()+"\\"+filename)
 
-            f.close()
-        else:
-            continue
+                f = open(filename, "r", encoding='utf-8')
+                data = f.read()
+                words_pos = tagger.pos(data)
+
+                for word in words_pos:
+                    noun = word[0]
+                    category = word[1]
+                    if (category == 'NNG'):
+                        if noun in hash:
+                            hash[noun] = hash[noun] + 1
+                        else:
+                            hash[noun] = 1
+                f.close()
+            else:
+                continue
+
+        os.chdir("../")
+
+    sortedArr = sorted(hash.items(), key=operator.itemgetter(1), reverse=True)
 
     os.chdir("../../3.nouns/")
-    f = open(subject+'.txt', "a")
-    for key, value in hash.items():
-        f.write(key +" "+ str(value)+"\n")
+    f = open(subject+'.txt', "w")
+    # for key, value in hash.items():
+    #     f.write(key +" "+ str(value)+"\n")
+
+    for item in sortedArr:
+        f.write(item[0] + " " + str(item[1]) + "\n")
 
     f.close()
 
