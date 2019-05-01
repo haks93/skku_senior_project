43b835035be021ce33bdfec55133f600816092e5
YusinShin <yusinshin@gmail.com>
2019-05-02 02:52:40 +0900

    hwp to txt 변환 끝.
    3. nouns 폴더 내용은 pyeunjeon.py 내용 수정하여 작성 예정.

--- src/pyeunjeon.py
+++ src/pyeunjeon.py
@@ -3,17 +3,17 @@ import os
 import operator
 
 def pyeunjeon(subjectIndex):
-    # 0: kor, 1: eng,  2: math, 3: physics, 4:chemical, 5: bio, 6: earth
-    subj = ["korean", "english", "mathematics", "sci_physics", "sci_chemical", "sci_bioscience", "sci_earthscience"]
+    # 0: kor, 1: eng,  2: math, 3: science
+    subj = ["korean", "english", "mathematics", "science"]
 
     subject = subj[subjectIndex]
 
     os.chdir("../data/txt/2.txt_files/"+subject)
     tagger = Mecab()
-    hash = {}
+    nouns_hash = {}
 
-    for filename2 in  os.listdir(os.getcwd()):
-        os.chdir("./" + filename2)
+    for subsection in  os.listdir(os.getcwd()):
+        os.chdir("./" + subsection)
 
         for filename in os.listdir(os.getcwd()):
             if filename.endswith(".txt"):
@@ -26,18 +26,23 @@ def pyeunjeon(subjectIndex):
                 for word in words_pos:
                     noun = word[0]
                     category = word[1]
-                    if (category == 'NNG'):
-                        if noun in hash:
-                            hash[noun] = hash[noun] + 1
+                    if (category == "NNG"):
+                        if noun in nouns_hash:
+                            nouns_hash[noun] = nouns_hash[noun] + 1
                         else:
-                            hash[noun] = 1
+                            nouns_hash[noun] = 1
                 f.close()
+
+                # 여기에서 각 파일별로 txt파일 출력할 수 있게 수정. 경로 설정이 번거로울듯.
+                os.chdir("../../../3.nouns/" + subject + "/" + subsection)
+                # 진짜 이렇게 왔다갔다 해야해..?
+
             else:
                 continue
 
         os.chdir("../")
 
-    sortedArr = sorted(hash.items(), key=operator.itemgetter(1), reverse=True)
+    sortedArr = sorted(nouns_hash.items(), key=operator.itemgetter(1), reverse=True)
 
     os.chdir("../../3.nouns/")
     f = open(subject+'.txt', "w", encoding='utf-8')
@@ -50,7 +55,7 @@ def pyeunjeon(subjectIndex):
     f.close()
 
 
-    return hash
+    return nouns_hash
 
 if __name__ == "__main__":
 
